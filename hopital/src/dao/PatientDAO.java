a9a6f4337cf3b8e5bcca6d423d99f8a35206bca8
Theo Chastenet <theo.chastenet@gadz.org>
2022-01-14 10:40:03 +0100

    Add Class Patient , DAO Patient

---
+++ hopital/src/dao/PatientDAO.java
@@ -0,0 +1,164 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Patient;
+
+public class PatientDAO implements IDAO<Patient, Integer>{
+
+	@Override
+	public Patient findById(Integer id) {
+		// TODO 
+
+		Patient p = null;
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager
+					.getConnection(urlBdd, loginBdd, passwordBdd);
+
+			PreparedStatement ps = conn.prepareStatement("SELECT * from patient where id=?");
+			ps.setInt(1,id);
+
+			ResultSet rs = ps.executeQuery();
+
+
+			while (rs.next())
+			{
+
+				
+				p = new Patient(rs.getInt("id"), rs.getString("nom"), rs.getString("prenom"));
+				
+			}
+			rs.close();
+			ps.close();
+			conn.close();
+
+
+
+		} catch (ClassNotFoundException | SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return p;
+		
+	}
+
+	@Override
+	public List<Patient> findAll() {
+		
+		List<Patient> patients = new ArrayList();
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(urlBdd, loginBdd, passwordBdd);
+
+			PreparedStatement ps = conn.prepareStatement("SELECT * from patient");
+			ResultSet rs = ps.executeQuery();
+
+			while(rs.next()) 
+			{	
+				
+				Patient p = new Patient(rs.getInt("id"), rs.getString("nom"), rs.getString("prenom"));
+				patients.add(p);
+				
+			}
+
+
+			rs.close();
+			ps.close();
+			conn.close();
+
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+
+
+		}
+
+		return patients;
+		
+	}
+
+	@Override
+	public void insert(Patient p) {
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(urlBdd, loginBdd, passwordBdd);
+		
+			PreparedStatement ps = conn.prepareStatement("INSERT INTO patient VALUES(?,?,?)", Statement.RETURN_GENERATED_KEYS);
+			ps.setInt(1,0);
+			ps.setString(2,p.getNom());
+			ps.setString(3,p.getPrenom());
+			
+			
+			ResultSet rs = ps.executeQuery();
+			
+			rs.close();
+			ps.close();
+			conn.close();
+		}
+		
+		
+		catch (ClassNotFoundException | SQLException e) {
+		e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void update(Patient p) {
+		// TODO Auto-generated method stub
+		try 
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(urlBdd, loginBdd, passwordBdd);
+			PreparedStatement ps = conn.prepareStatement("Update patient set nom=?,prenom=? where id=?");
+			
+			ps.setString(1,p.getNom());
+			ps.setString(2,p.getPrenom());
+			ps.setObject(3,  p.getId());
+			
+			ps.executeUpdate();
+			
+			
+			ps.close();
+			conn.close();
+		}
+		catch (ClassNotFoundException | SQLException e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+		try 
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(urlBdd, loginBdd, passwordBdd);
+			
+			PreparedStatement ps = conn.prepareStatement("delete from patient where id=?");
+			ps.setInt(1, id);
+			
+			ps.executeUpdate();
+			
+			ps.close();
+			conn.close();
+		
+		}
+		catch (ClassNotFoundException | SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
