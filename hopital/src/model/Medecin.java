f56658448e43cd6bcc94a7141295b358b85e8c90
Vdellac <vdellac@outlook.fr>
2022-01-14 10:12:55 +0100

    ajout classes + DAO findById

---
+++ hopital/src/model/Medecin.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Medecin extends Compte{
+
+	public Medecin(Integer id, String login, String password ) {
+		super(id, login, password);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ hopital/src/model/Secretaire.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Secretaire extends Compte{
+
+	public Secretaire(Integer id, String login, String password ) {
+		super(id, login, password);
+		// TODO Auto-generated constructor stub
+	}
+
+}
