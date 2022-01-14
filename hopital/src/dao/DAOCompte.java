3dc28167b01cab2322e34dcc1dc7bb4b078a4b4a
Vdellac <vdellac@outlook.fr>
2022-01-14 10:44:57 +0100

    add Features DAOCompte

--- hopital/src/dao/DAOCompte.java
+++ hopital/src/dao/DAOCompte.java
@@ -1,6 +1,7 @@
 package dao;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import model.Compte;
@@ -16,49 +17,140 @@ public class DAOCompte implements IDAO<Compte, Integer> {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection conn = DriverManager.getConnection(urlBdd,loginBdd,passwordBdd);
 
-			PreparedStatement ps = conn.prepareStatement("SELECT * FROM compte where id=?");
+			PreparedStatement ps = conn.prepareStatement("SELECT * FROM compte WHERE id=?");
 			ps.setInt(1, id);
 			ResultSet rs = ps.executeQuery();
 
-			while (rs.next()){
-				if (rs.getString("type_compte").equals("Medecin")) {
-					c = new Medecin (rs.getInt("id"),rs.getString("login"),rs.getString("password"))
-							;			}
-				else if (rs.getString("type_compte").equals("Medecin")) {
-					c = new Secretaire (rs.getInt("id"),rs.getString("login"),rs.getString("password"))
-
+			while (rs.next())
+			{
+				if (rs.getString("type_compte").equals("Medecin")) 
+				{
+					c = new Medecin (rs.getInt("id"),rs.getString("login"),rs.getString("password"));
 				}
-
+				else if (rs.getString("type_compte").equals("Secretaire"))
+				{
+					c = new Secretaire (rs.getInt("id"),rs.getString("login"),rs.getString("password"));
+				}
 			}
-			catch(Exception e) {
-				e.printStackTrace();
-			}
-
-			return c;
+			rs.close();
+			ps.close();
+			conn.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return c;
 	}
+
 
 	@Override
 	public List<Compte> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Compte> comptes = new ArrayList();
+
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(urlBdd,loginBdd,passwordBdd);
+
+			PreparedStatement ps = conn.prepareStatement("SELECT * FROM compte");
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next())
+			{
+				if (rs.getString("type_compte").equals("Medecin")) 
+				{
+					Medecin m = new Medecin (rs.getInt("id"),rs.getString("login"),rs.getString("password"));
+					comptes.add(m);
+				}
+				else if (rs.getString("type_compte").equals("Secretaire"))
+				{
+					Secretaire s = new Secretaire (rs.getInt("id"),rs.getString("login"),rs.getString("password"));
+					comptes.add(s);
+				}
+
+			}
+			rs.close();
+			ps.close();
+			conn.close();
+		}
+		catch (Exception e) { 
+			e.printStackTrace();
+		}
+
+		return comptes;
 	}
 
 	@Override
 	public void insert(Compte o) {
-		// TODO Auto-generated method stub
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(urlBdd,loginBdd,passwordBdd);
+
+			PreparedStatement ps = conn.prepareStatement("INSERT INTO compte (id, login, password, type_compte) VALUES (?,?,?,?)");
+			ps.setInt(1, o.getId());
+			ps.setString(2, o.getLogin());
+			ps.setString(3, o.getPassword());
+			
+			if (o instanceof Medecin) {
+				ps.setString(4, "Medecin");
+			}
+			else if (o instanceof Secretaire) {
+				ps.setString(4, "Secretaire");
+			}
+			
+			ps.executeUpdate();
+			ps.close();
+			conn.close();
+
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
 	public void update(Compte o) {
-		// TODO Auto-generated method stub
-		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(urlBdd,loginBdd,passwordBdd);
+
+			PreparedStatement ps = conn.prepareStatement("UPDATE compte SET login=? ,password=? WHERE id=?");
+			ps.setInt(3, o.getId());
+			ps.setString(1, o.getLogin());
+			ps.setString(2, o.getPassword());
+			ps.executeUpdate();
+
+			ps.close();
+			conn.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
 	public void delete(Integer id) {
-		// TODO Auto-generated method stub
-		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(urlBdd,loginBdd,passwordBdd);
+
+			PreparedStatement ps = conn.prepareStatement("DELETE FROM compte WHERE id=?");
+			ps.setInt(1, id);
+			ps.executeUpdate();
+
+			ps.close();
+			conn.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
 	}
 
 }
